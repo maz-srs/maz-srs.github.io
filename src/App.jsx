f610f9cf16a6c448102af82e702665b865d7a25b
NovaActias <gamedev.nugget750@passinbox.com>
2026-02-25 22:26:10 +0100

    win95 style added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 function App() {
-  return <div>Hello</div>
+  return (
+    <div className="window" style={{ width: 300, margin: 50 }}>
+      <div className="title-bar">
+        <div className="title-bar-text">My Portfolio</div>
+      </div>
+      <div className="window-body">
+        <p>Hello!</p>
+      </div>
+    </div>
+  )
 }
 
 export default App
